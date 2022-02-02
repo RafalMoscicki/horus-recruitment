ca400e3d68d0bef2488e0c0e8d12fc94fdc63b98
Rafal Moscicki <moscicki.rafal@gmail.com>
2022-02-03 00:59:59 +0100

    Improvements, tests, null safe

--- src/main/java/pl/rmoscicki/horus/recruitment/Wall.java
+++ src/main/java/pl/rmoscicki/horus/recruitment/Wall.java
@@ -1,5 +1,7 @@
 package pl.rmoscicki.horus.recruitment;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -14,34 +16,43 @@ public class Wall implements Structure {
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
+        if (color == null) {
+            return Optional.empty();
+        }
         return blocks.stream()
-                .filter(block -> block.getColor().equals(color))
+                .flatMap(block -> getAllBlocks(block).stream())
+                .filter(block -> color.equals(block.getColor()))
                 .findAny();
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
+        if (material == null) {
+            return Collections.emptyList();
+        }
         return blocks.stream()
-                .filter(block -> block.getMaterial().equals(material))
+                .flatMap(block -> getAllBlocks(block).stream())
+                .filter(block -> material.equals(block.getMaterial()))
                 .collect(Collectors.toList());
     }
 
     @Override
     public int count() {
         return blocks.stream()
-                .map(this::count)
-                .mapToInt(x -> x)
+                .mapToInt(block -> getAllBlocks(block).size())
                 .sum();
     }
 
-    private int count(Block block) {
+    private List<Block> getAllBlocks(Block block) {
+        List<Block> result = new LinkedList<>();
+        result.add(block);
         if (block instanceof CompositeBlock) {
-            return ((CompositeBlock) block).getBlocks()
+            List<Block> children = ((CompositeBlock) block).getBlocks()
                     .stream()
-                    .map(this::count)
-                    .mapToInt(x -> x)
-                    .sum();
+                    .flatMap(b -> getAllBlocks(b).stream())
+                    .collect(Collectors.toList());
+            result.addAll(children);
         }
-        return 1;
+        return result;
     }
 }
--- src/test/java/pl/rmoscicki/horus/recruitment/WallTest.java
+++ src/test/java/pl/rmoscicki/horus/recruitment/WallTest.java
@@ -1,14 +1,14 @@
 package pl.rmoscicki.horus.recruitment;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -17,8 +17,7 @@ class WallTest {
     @Test
     void shouldCountForEmptyWall() {
         //Given
-        List<Block> blocks = Collections.emptyList();
-        Structure wall = new Wall(blocks);
+        Structure wall = createEmptyWall();
 
         //When
         int count = wall.count();
@@ -30,9 +29,7 @@ class WallTest {
     @Test
     void shouldCountForOneSimpleBlock() {
         //Given
-        Block simpleBlock = mock(Block.class);
-        List<Block> blocks = Arrays.asList(simpleBlock);
-        Structure wall = new Wall(blocks);
+        Structure wall = createWallWithOneSimpleBlock();
 
         //When
         int count = wall.count();
@@ -44,33 +41,243 @@ class WallTest {
     @Test
     void shouldCountForCompositeBlock() {
         //Given
-        CompositeBlock compositeBlock = mock(CompositeBlock.class);
-        when(compositeBlock.getBlocks()).thenReturn(Arrays.asList(mock(Block.class), mock(Block.class)));
-        List<Block> blocks = Arrays.asList(compositeBlock);
-        Structure wall = new Wall(blocks);
-
-        //When
-        int count = wall.count();
-
-        //Then
-        assertEquals(2, count);
-    }
-
-    @Test
-    void shouldCountForNestedCompositeBlock() {
-        //Given
-        CompositeBlock compositeBlock = mock(CompositeBlock.class);
-        CompositeBlock nestedCompositeBlock = mock(CompositeBlock.class);
-        when(nestedCompositeBlock.getBlocks()).thenReturn(Arrays.asList(mock(Block.class), mock(Block.class)));
-        when(compositeBlock.getBlocks()).thenReturn(Arrays.asList(mock(Block.class), nestedCompositeBlock));
-        List<Block> blocks = Arrays.asList(compositeBlock);
-        Structure wall = new Wall(blocks);
+        Structure wall = createWallWithOneCompositeBlock();
 
         //When
         int count = wall.count();
 
         //Then
         assertEquals(3, count);
+    }
+
+    @Test
+    void shouldCountForCompositeNestedBlock() {
+        //Given
+        Structure wall = createWallWihOneCompositeNestedBlock();
+
+        //When
+        int count = wall.count();
+
+        //Then
+        assertEquals(5, count);
+    }
+
+    @Test
+    void shouldNotFindByColorForEmptyWall() {
+        //Given
+        Structure wall = createEmptyWall();
+
+        //When
+        Optional<Block> result = wall.findBlockByColor("red");
+
+        //Then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldFindByColorForOneSimpleBlock() {
+        //Given
+        Structure wall = createWallWithOneSimpleBlock();
+
+        //When
+        Optional<Block> result = wall.findBlockByColor("red");
+
+        //Then
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    void shouldNotFindByColorForOneSimpleBlock() {
+        //Given
+        Structure wall = createWallWithOneSimpleBlock();
+
+        //When
+        Optional<Block> result = wall.findBlockByColor("pink");
+
+        //Then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldFindByColorForCompositeBlock() {
+        //Given
+        Structure wall = createWallWithOneCompositeBlock();
+
+        //When
+        Optional<Block> result = wall.findBlockByColor("red");
+
+        //Then
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    void shouldNotFindByColorForCompositeBlock() {
+        //Given
+        Structure wall = createWallWithOneCompositeBlock();
+
+        //When
+        Optional<Block> result = wall.findBlockByColor("pink");
+
+        //Then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldFindByColorForCompositeNestedBlock() {
+        //Given
+        Structure wall = createWallWihOneCompositeNestedBlock();
+
+        //When
+        Optional<Block> result = wall.findBlockByColor("red");
+
+        //Then
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    void shouldNotFindByColorForCompositeNestedBlock() {
+        //Given
+        Structure wall = createWallWihOneCompositeNestedBlock();
+
+        //When
+        Optional<Block> result = wall.findBlockByColor("pink");
+
+        //Then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldNotFindByMaterialForEmptyWall() {
+        //Given
+        Structure wall = createEmptyWall();
+
+        //When
+        List<Block> result = wall.findBlocksByMaterial("airbrick");
+
+        //Then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldFindByMaterialForOneSimpleBlock() {
+        //Given
+        Structure wall = createWallWithOneSimpleBlock();
+
+        //When
+        List<Block> result = wall.findBlocksByMaterial("brick");
+
+        //Then
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void shouldNotFindByMaterialForOneSimpleBlock() {
+        //Given
+        Structure wall = createWallWithOneSimpleBlock();
+
+        //When
+        List<Block> result = wall.findBlocksByMaterial("airbrick");
+
+        //Then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldFindByMaterialForCompositeBlock() {
+        //Given
+        Structure wall = createWallWithOneCompositeBlock();
+
+        //When
+        List<Block> result = wall.findBlocksByMaterial("brick");
+
+        //Then
+        assertEquals(3, result.size());
+    }
+
+    @Test
+    void shouldNotFindByMaterialForCompositeBlock() {
+        //Given
+        Structure wall = createWallWithOneCompositeBlock();
+
+        //When
+        List<Block> result = wall.findBlocksByMaterial("airbrick");
+
+        //Then
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldFindByMaterialForCompositeNestedBlock() {
+        //Given
+        Structure wall = createWallWihOneCompositeNestedBlock();
+
+        //When
+        List<Block> result = wall.findBlocksByMaterial("brick");
+
+        //Then
+        assertEquals(4, result.size());
+    }
+
+    @Test
+    void shouldNotFindByMaterialForCompositeNestedBlock() {
+        //Given
+        Structure wall = createWallWihOneCompositeNestedBlock();
+
+        //When
+        List<Block> result = wall.findBlocksByMaterial("airbrick");
+
+        //Then
+        assertTrue(result.isEmpty());
+    }
+
+    private Structure createEmptyWall() {
+        List<Block> blocks = Collections.emptyList();
+        return new Wall(blocks);
+    }
+
+    private Structure createWallWithOneSimpleBlock() {
+        Block simpleBlock = mock(Block.class);
+        when(simpleBlock.getColor()).thenReturn("red");
+        when(simpleBlock.getMaterial()).thenReturn("brick");
+        List<Block> blocks = Arrays.asList(simpleBlock);
+        return new Wall(blocks);
+    }
+
+    private Structure createWallWithOneCompositeBlock() {
+        CompositeBlock compositeBlock = mock(CompositeBlock.class);
+        when(compositeBlock.getColor()).thenReturn("red");
+        when(compositeBlock.getMaterial()).thenReturn("brick");
+        Block block1 = mock(Block.class);
+        Block block2 = mock(Block.class);
+        when(block1.getColor()).thenReturn("red");
+        when(block1.getMaterial()).thenReturn("brick");
+        when(block2.getColor()).thenReturn("white");
+        when(block2.getMaterial()).thenReturn("brick");
+        when(compositeBlock.getBlocks()).thenReturn(Arrays.asList(block1, block2));
+        List<Block> blocks = Arrays.asList(compositeBlock);
+        return new Wall(blocks);
+    }
+
+    private Structure createWallWihOneCompositeNestedBlock() {
+        Block block1 = mock(Block.class);
+        Block block2 = mock(Block.class);
+        Block block3 = mock(Block.class);
+        when(block1.getColor()).thenReturn("red");
+        when(block1.getMaterial()).thenReturn("brick");
+        when(block2.getColor()).thenReturn("white");
+        when(block2.getMaterial()).thenReturn("brick");
+        when(block3.getColor()).thenReturn("white");
+        when(block3.getMaterial()).thenReturn("brick");
+        CompositeBlock compositeBlock = mock(CompositeBlock.class);
+        CompositeBlock nestedCompositeBlock = mock(CompositeBlock.class);
+        when(compositeBlock.getColor()).thenReturn("red");
+        when(compositeBlock.getMaterial()).thenReturn("brick");
+        when(nestedCompositeBlock.getColor()).thenReturn(null);
+        when(nestedCompositeBlock.getMaterial()).thenReturn(null);
+        when(nestedCompositeBlock.getBlocks()).thenReturn(Arrays.asList(block1, block2));
+        when(compositeBlock.getBlocks()).thenReturn(Arrays.asList(block3, nestedCompositeBlock));
+        List<Block> blocks = Arrays.asList(compositeBlock);
+        return new Wall(blocks);
     }
 
 }
